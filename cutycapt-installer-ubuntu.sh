bad27d64b6d4ee5e9d431d0d36c5bfb235cf7f9c
ehime <dodomeki@gmail.com>
2013-09-20 13:50:59 -0700

    Adding Cuty installers for RHEL and DEB

---
+++ cutycapt-installer-ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "Now installing CutyCapt"
+
+sudo apt-get update -y
+sudo apt-get install -y build-essential
+sudo apt-get install -y xvfb
+sudo apt-get install -y xfs xfonts-scalable xfonts-100dpi
+sudo apt-get install -y libgl1-mesa-dri
+sudo apt-get install -y subversion libqt4-webkit libqt4-dev g++
+
+mkdir ~/scripts
+cd ~/scripts
+svn co https://svn.code.sf.net/p/cutycapt/code/ cutycapt && cd cutycapt/CutyCapt
+
+# needs patching
+echo 'Index: CutyCapt.hpp
+===================================================================
+--- CutyCapt.hpp	(revision 10)
++++ CutyCapt.hpp	(working copy)
+@@ -1,4 +1,6 @@
+ #include <QtWebKit>
++#include <QNetworkReply>
++#include <QSslError>
+ 
+ #if QT_VERSION >= 0x050000
+ #include <QtWebKitWidgets>
+' > CutyCapt.patch
+
+patch -p0 -i CutyCapt.patch
+
+qmake
+make
+
+ln -s ~/scripts/cutycapt/CutyCapt/CutyCapt /bin
+
+xvfb-run --server-args="-screen 0, 1024x768x24" CutyCapt --url=http://www.google.com --out=example.png
