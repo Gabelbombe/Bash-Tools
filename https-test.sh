e8abb44bc1d168de2b49638abe4bfcd896c7807e
ehime <dodomeki@gmail.com>
2013-11-11 11:22:11 -0800

    NL for ternary

--- https-test.sh
+++ https-test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 site='tldp.org'		# http
-site='erado.com'	# https
+#site='erado.com'	# https
 
-[ '' != "$(echo $(echo ^D | telnet $site https 2> /dev/null) | awk '{print $3}')" ] && protocol='https' || protocol='http'
+# are you SSL verified or not ... ?
+[ '' != "$(echo $(echo ^D | telnet $site https 2> /dev/null) | awk '{print $3}')" ] \
+	&& protocol='https' \
+	|| protocol='http'
 
 echo $protocol
