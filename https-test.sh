ac899d61dc811356f90ac68b2bb904a3142c3045
ehime <dodomeki@gmail.com>
2013-11-11 11:28:18 -0800

    Adding optional domain testing w default fallback

--- https-test.sh
+++ https-test.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# Test for SSL verified site
+ 
+# CPR : Jd Daniel :: Ehime-ken
+# MOD : 2013-11-11 @ 11:23:30
+# INP : $ ./https-test.sh {optional: domain.com}
 
-site='tldp.org'		# http
-#site='erado.com'	# https
+if [ ! -z $1 ]; then
+	site="$1"
+else
+	site='tldp.org'		# http
+	#site='erado.com'	# https
+fi
 
 # are you SSL verified or not ... ?
 [ '' != "$(echo $(echo ^D | telnet $site https 2> /dev/null) | awk '{print $3}')" ] \
