b1f58157d1b544060276700af616921d3f5deae8
ehime <dodomeki@gmail.com>
2014-04-02 15:50:43 -0700

    Making it saner and adding some notations or some shit

--- git-whitespaced-passwords.sh
+++ git-whitespaced-passwords.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-    read -p "Enter STASH Username: " user
-    read -p "Enter STASH Password: " pass
+## Get user creds and stuff    
+read -p "Enter STASH Username: " user
+read -p "Enter STASH Password: " pass
 
-    repos=( BenApp BenSrv BenShuttle )
+## Current repos as an array 
+repos=( BenApp BenSrv BenShuttle )
 
-	git config --global http.postBuffer 2G
+## Push up the limit so we won't choke
+git config --global http.postBuffer 2G
 
-    for i in "${repos[@]}"; do
-	    repo=$(echo "git clone 'https://${user}:${pass}@stash.corbis.com/scm/bnr/$(echo $i | awk '{print tolower($0)}').git' $i/")
-	    eval "$(echo $repo)"
-    done
+## pathectory to clone to, can be .
+read -p "What is your clone path: " path
 
+## Make 
+[ -d "${path}" ] || { 
+    mkdir -p "${path}" ;
+}
 
+## Go
+cd "${path}"
+
+## Do it
+for i in "${repos[@]}"; do
+    repo=$(echo "git clone 'https://${user}:${pass}@stash.corbis.com/scm/bnr/$(echo $i | awk '{print tolower($0)}').git' $i/")
+    eval "$(echo $repo)"
+done
