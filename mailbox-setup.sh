195ffd4ebc937c77aaf2fda7aeea9068aa7cfd2e
ehime <dodomeki@gmail.com>
2014-06-16 11:51:23 -0700

    Mutt and mailer setup

---
+++ mailbox-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Set up a local mailer and inbox with MUTT
+#
+# CPR : Jd Daniel :: Ehime-ken
+# MOD : 2014-06-16 @ 11:48:48
+# VER : 1a
+
+## ROOT check
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as su" 1>&2 ; exit 1
+fi
+
+
+apt-get install -y mutt mailutils
+touch /var/mail/$USER
+chown $USER:mail /var/mail/$USER
+
+chmod o-r /var/mail/$USER
+chmod g+rw /var/mail/$USER
+
+read -p 'Send testmail to: ' email
+
+echo "Test email from $(echo $USER)" | mutt -s 'Working' -- $email
+exit 0
