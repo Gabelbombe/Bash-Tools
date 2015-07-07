0e40178bdc4e6aeae2099a1fc3fa55dbd84c36f2
ehime <dodomeki@gmail.com>
2015-07-06 18:17:27 -0700

    Changing PXE loader to prevent yelling

---
+++ GIT/git-graft-branch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+from="$1"
+b1="$2"
+
+to="$3"
+b2="$4"
+
+[[ -z "$fr" ]] || [[ -z "$to" ]] || [[ -z "$b1" ]] || [[ -z "$b2" ]] && {
+  echo "$0 [Repo From] -> [Branch 1] | [Repo To] -> [Branch 2]"
+  exit 1
+}
+
+cd /tmp
+
+echo -e '\nPulling...\n'
+git clone --bare "$fr" from/
+cd from
+
+echo -e '\nGrafting...\n'
+git checkout "$b1" "remotes/$b1"
+
+git push  --mirror   "$to"
+rm -fr /tmp/from
+
+echo -e '\nSuccess!!'
