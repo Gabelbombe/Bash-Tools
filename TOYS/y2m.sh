52f9f4c0000dd6b10bb37a3b1b4fe48817635c07
ehime <dodomeki@gmail.com>
2015-07-27 11:55:07 -0700

    Rematch update allows any file extension after download, no longer preferred

--- TOYS/y2m.sh
+++ TOYS/y2m.sh
@@ -2,19 +2,22 @@
 # Youtube to MP3 Bash Script
 
 # CPR : Jd Daniel :: Ehime-ken
-# MOD : 2015-01-21 @ 16:11:44
-# VER : Version 5 (OSX Darwin)
+# MOD : 2015-07-27 @ 11:53:05
+# VER : Version 6 (OSX Darwin)
 
 # REQ : http://developer.echonest.com/docs/v4/song.html
 
+##  ##  ##  ##
+##  ##  ##  ##
 
-# no long-opts supported except --help
+
+## no long-opts supported except --help
 while getopts 'v:d:i:-:' OPT; do
   case $OPT in
     v) address=$OPTARG;;
     d) dir=$OPTARG;;
     i) image=$OPTARG ; eval image=$image;;
-    -) #long option
+    -) ## long option
     case $OPTARG in
 
       help) echo 'Long:  y2m -d {directory} -v {ex: http://www.youtube.com/watch?v=oHg5SJYRHA0‎}'
@@ -28,9 +31,9 @@ done
 
 cd /tmp
 
-# if short order (y2m http://addy.com)
+## if short order (y2m http://addy.com)
 [ -e $address ] && {
-  # default to /home/{user}/Music
+  ## default to /home/{user}/Music
   address=$1; dir="/Users/${USER}/Music/"
 }
 
@@ -43,33 +46,36 @@ regex='v=(.*)'
   filename=$(basename "$FILEPATH")
   extension="${filename##*.}"
 
-  # remove thumb if exists
+  ## remove thumb if exists
   [ -f 'thumbnail.jpg' ] && {
     rm -f 'thumbnail.jpg'
   }
 
-  # get/set thumbnail for MP3
+  ## get/set thumbnail for MP3
   [ ! -f "${image}" ] && {
     youtube-dl --no-warnings --write-thumbnail $address -o thumbnail.jpg
   } || {
     cp -ir "${image}" thumbnail.jpg
   }
 
+  ## get title to start off with...
   video_title="$(youtube-dl --no-warnings --get-title $address |sed s/://g)"
+
   artist="$(echo $video_title |awk -F '-' '{print$1}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')"
   title="$(echo $video_title |awk -F '-' '{print$2}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')"
+  video=$(ls |grep "$video_title") ## format independant, might need: head -n1
 
   # download the FLV stream
   youtube-dl --no-warnings -o "$video_title" $address
 
-  ffmpeg -i "$video_title".mp4         \
+  ffmpeg -i "$video_type"              \
         -acodec libmp3lame             \
         -ab 320k                       \
         -ac 2                          \
         -vn                            \
         -y "$video_title".mp3
 
-  # add image with LAME since FFMPEG changes too much....
+  ## add image with LAME since FFMPEG changes too much....
   lame --preset insane -V0 --id3v2-only --ignore-tag-errors \
        --ti 'thumbnail.jpg'                                 \
        --ta "$artist"                                       \
