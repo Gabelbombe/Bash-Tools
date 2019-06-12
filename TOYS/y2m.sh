933faecd898e78bf15472c21b810c86a8c0c891d
ehime <dodomeki@gmail.com>
2019-06-12 15:44:03 -0700

    threw away crap casing for flags, threw away nest crap that no longer exists as a company... did some other shit, im pretty sure.... probably broke the internet in the proc.... .#prost

--- TOYS/y2m.sh
+++ TOYS/y2m.sh
@@ -1,118 +1,141 @@
 #!/bin/bash -x
-# Youtube to MP3 Bash Script
+# Youtube to MP3 BASH script to steal shit...
 
-CPR='Jd Daniel :: Ehime-ken'
-MOD=$(date +"%Y-%m-%d @ %H:%M:%S")
-VER='7.0.4'
+CPR='Jd Daniel :: Gabelbombe'
+MOD="$(date +'%Y-%m-%d @ %H:%M:%S')"
+VER='7.1.0'
 
-# REF : https://github.com/ehime/Bash-Tools/blob/master/TOYS/y2m.sh
-# REQ : http://developer.echonest.com/docs/v4/song.html
+# REF : https://github.com/Gabelbombe/Bash-Tools/blob/master/TOYS/y2m.sh
 # REQ : https://github.com/aadsm/JavaScript-ID3-Reader
 
-# Reading tags via: brew install eyed3
-# $ eyeD3 Black\ Rebel\ Motorcycle\ Club\ -\ Red\ Eyes\ And\ Tears.mp3
-# Black Rebel Motorcycle Club - Red Eyes And Tears.mp3    [ 8.09 MB ]
-# -------------------------------------------------------------------------------
-# Time: 03:58    MPEG1, Layer III    [ ~283 kb/s @ 44100 Hz - Joint stereo ]
-# -------------------------------------------------------------------------------
-# ID3 v2.3:
-# title: Red Eyes And Tears
-# artist: Black Rebel Motorcycle Club
-# album:
-# album artist: Black Rebel Motorcycle Club
-# track:
-# Comment: [Description: ] [Lang: XXX]
-#
-# CPR: Jd Daniel :: Ehime-ken
-# MOD: 2019-06-12 @ 13:29:46
-# VER: Version 7.0.4 (OSX Maverick)
-# REF: https://www.youtube.com/watch?v=kOdHND_wt0k
-#
-# OTHER Image: [Size: 15747 bytes] [Type: image/jpeg]
-# Description:
-#
-# -------------------------------------------------------------------------------
-
-## We can keep MP3's in new working version now with the following code
-##
-# function version { echo "$@" |gawk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; }
-# if [ "$(version $(eyeD3 $SONG |grep '\[VER\]' |awk '{print$2}'))" -gt bash $(which y2m |head -n1 |awk '{print$3}') --version ]; then
-#   bash $(which y2m |head -n1 |awk '{print$3}') "$(version $(eyeD3 $SONG |grep '\[REF\]' |awk '{print$2}'))"
-# fi
+##  Buyers beware....
+##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##
+##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##
 
 
-##  ##  ##  ##
-##  ##  ##  ##
+declare debug=''
+declare title=''
+declare isurl='http?(s)://*'
 
-debug=''
-video_title=''
+function print_usage()
+{
+  echo -e '
+  Parameter usage: y2m [--help] [--title=<song-title>] [--video=<youtube-url>] [--cover=<cover-${save}>] [--save=<save-location>]
+  Parameters:
+    -t  --title   Title of the MP3 you want, should conform to `Art - Song Name`
+    -v  --video   Video URL to fetch from YouTube
+    -c  --cover   ${save} to attach to the MP3 [ Defaults to YouTube Promo ${save} ]
+    -d  --save    Directory save location    [ Defaults to `~/Music` ]
+    -v  --version Displays current version of y2m
 
-## TODO: Clean up  optargs since its ugly and could be a lot better
-## TODO: github.com/ehime/Bash-Tools/blob/master/GIT/git-backup.sh
+    Advanced Parameters:
+        --debug   Appends `sketchy debugging` to the script, trust it like youd trust a white girl...
+        --update  Force updates `youtube-dl`, if you err out, run this...
+        --flush   Flushes `youtube-dl` caches
 
-## no long-opts supported except --help
-while getopts 'v:d:t:i:-:' OPT; do
-  case $OPT in
-    t) video_title="${OPTARG}";;
-    v) address="${OPTARG}";;
-    d) dir=$OPTARG;;
-    i) image=$OPTARG ; eval image=$image;;
-    -) ## long option
-    case $OPTARG in
+  Example usage:
+    Long:  y2m -t "Rick Astley - Never Gonna Give You Up" -s /dev/null -v http://www.youtube.com/watch?v=oHg5SJYRHA0‎"
+    Short: y2m http://www.youtube.com/watch?v=oHg5SJYRHA0‎ ## just pirate the fucking thing already...
+  '
+}
 
-      help) echo 'Long:  y2m -d {directory} -v {ex: http://www.youtube.com/watch?v=oHg5SJYRHA0‎}'
-            echo 'Short: y2m {ex: http://www.youtube.com/watch?v=oHg5SJYRHA0‎}'
-            return
-      ;;
+## following requires modern GNU bash 3.2 you loser....
+if (shopt -s nocasematch ; [[ ${1} = @(-h|--help) ]]) ; then
+  print_usage ; return 1
 
-      ## continues with argv[2] as address...
-      debug) debug='--verbose --print-traffic --dump-pages'
-             address="${2}"
-      ;;
+## because you're a lazy cunt...
+elif [[ [[ "${1}" =~ $isurl ]] ; then
+  title="${1}"
 
-      flush) echo "[info] Flushing caches..."
-             youtube-dl --no-check-certificate --rm-cache-dir
-             return
-      ;;
+else
+  while [[ $# -gt 0 ]]; do
+    opt="${1}" ; shift ;
 
-      version) echo "$VER" ; return
+    ## no one likes a smart-ass....
+    current_arg="${1}"
 
-    esac;;
-  esac
-done
+    if [[ "${current_arg}" =~ ^-{0,1}.* ]] ; then
+      echo -e "[fatal] The Universe doesn't give a fuck about your feelings..."
+    fi
 
-cd /tmp
+    case "${opt}" in
+      "-t"|"--title"      ) title="${1}" ; shift ;;
+      "-v"|"--video"      ) video="${1}" ; shift ;;
+      "-c"|"--cover"      ) ${save}="${1}" ; shift ;;
+      "-s"|"--save"       ) save="${1}"  ; shift ;;
+
+      ## version out...
+      "-v"|"--version"    ) echo -e "[info] Current verion is ${VER}"
+                            return 1
+                          ;;
+
+      ## advanced flags, buyer beware...
+      "--debug"           ) echo -e "[warn] Activating sketchy dumping..."
+                            debug='--verbose --print-traffic --dump-pages'
+                            shift
+                          ;;
+
+      "--flush"           ) echo -e "[info] Flushing caches..."
+                            youtube-dl --no-check-certificate --rm-cache-dir
+                            return 1
+
+      "--update"          ) echo -e "[info] Force updating youtube-dl..."
+                            sudo youtube-dl -U
+                            return 1
+
+
+      ## you sir, just boiled the fuckin ocean..
+      *                   ) echo -e "[fatal] Invalid option: \""$opt"\"" >&2
+                            return 6
+                          ;;
+    esac
+  done
+fi
+
+## unset usage from global scope
+unset -f print_usage
+
+
+## make a temporary directory and move into it....
+tmp_dir=$(mktemp -d -t y2m-XXXXXXXXXX) \
+  && cd "${tmp_dir}"
+
+echo -e "[info] Temporary directory is: ${tmp_dir}"
+
 
 ## TODO: implement for stripping / quoting
 function ere_quote () {
-    sed 's/[]\.|$(){}?+*^]/\\&/g' <<< "$*"
+  sed 's/[]\.|$(){}?+*^]/\\&/g' <<< "$*"
 }
 
 ## if short order (y2m http://addy.com)
-[ "x$address" == "x" ] && { address=$1 ; }
-[ "x$dir" == "x" ]     && { dir="/Users/${USER}/Music/" ; }
+[ "x${video}" == "x" ] && { video=${1} ; }
+[ "x${save}"  == "x" ] && { save="/Users/${USER}/Music/" ; }
 
-## dir exists?
-[ ! -d "${dir}" ]      && { echo "[error] Directory '${dir}' does not not exist..." ; return 1 ; }
+## save location exists?
+[ ! -d "${save}" ]     && {
+  echo -e "[fatal] Directory '${save}' does not not exist..."
+  return 9
+}
 
 ## set comments
-COMMENTS="
-[CPR] $CPR
-[MOD] $MOD
-[VER] $VER
-[REF] $address
+declare COMMENTS="
+[CPR] ${CPR}
+[MOD] ${MOD}
+[VER] ${VER}
+[REF] ${video}
 "
 
-echo "[info] Using directory: ${dir}"
+
+echo -e "[info] Using directory: ${save}"
 
 regex='v=(.*)'
-[[ $address =~ $regex ]] && {
+[[ "${video}" =~ $regex ]] && {
 
   ## argsmap
-  video_id=${BASH_REMATCH[1]}
-  video_id=$(echo $video_id | cut -d'&' -f1)
-
-  filename=$(basename "$FILEPATH")
+  video_id="${BASH_REMATCH[1]}"
+  video_id="$(echo $video_id| cut -d'&' -f1)"
+  filename=$(basename "${FILEPATH}")
   extension="${filename##*.}"
 
 
@@ -123,56 +146,56 @@ regex='v=(.*)'
 
 
   ## get/set thumbnail for MP3
-  if [[ "x$image" == "x" && ! -f "${image}" ]] ; then
-    echo "[info] Downloading thumbnail"
-    youtube-dl --no-check-certificate $debug --no-warnings --write-thumbnail $address -o thumbnail.jpg
+  if [[ "x$${save}" == "x" && ! -f "${${save}}" ]] ; then
+    echo -e "[info] Downloading thumbnail"
+    youtube-dl --no-check-certificate "${debug}" --no-warnings --write-thumbnail "${video}" -o thumbnail.jpg
   else
-    echo "[info] Using ${image} as thumbail"
-    cp -ir "${image}" thumbnail.jpg
+    echo -e "[info] Using ${${save}} as thumbail"
+    cp -ri "${${save}}" thumbnail.jpg
   fi
 
 
   ## if you haven't defined a title....
-  [ "x$video_title" == "x" ] && {
-    video_title="$(youtube-dl --no-check-certificate $debug --no-warnings --get-title $address |sed s/://g)"
+  [ "x${title}" == "x" ] && {
+    title="$(youtube-dl --no-check-certificate "${debug}" --no-warnings --get-title "${video}" |sed s/://g)"
   }
 
-  echo "[info] Title is: ${video_title}"
+  echo -e "[info] Title is: ${title}"
 
 
   # download the FLV stream
-  youtube-dl --no-check-certificate $debug --no-warnings -o "$video_title" $address
+  youtube-dl --no-check-certificate "${debug}" --no-warnings -o "${title}" "${video}"
+
+  artist="$(echo ${title} |awk -F ' - ' '{print${1}}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')"
+  title="$(echo ${title}  |awk -F ' - ' '{print$2}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')"
 
 
-  ## TODO: Add API lookup via Echonest
-  artist="$(echo $video_title |awk -F ' - ' '{print$1}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')"
-  title="$(echo $video_title  |awk -F ' - ' '{print$2}' |sed -e 's/\[.*//g' -e 's/  */ /g' -e 's/^ *\(.*\) *$/\1/')"
-  video=$(ls |grep "${artist}") ## format independant, might need: head -n1, also hates ( ) [ ] etc
+  ## format independant, might need: head -n1, also hates ( ) [ ] etc
+  video=$(ls |grep "${artist}")
 
-  echo "[info] Using Video: ${video}"
-
+  echo -e "[info] Using Video: ${video}"
 
   ## REQ: FFMPEG proper installers via
-  ## https://github.com/ehime/Bash-Tools/tree/master/STANDUP
+  ## https://github.com/Gabelbombe/Bash-Tools/tree/master/STANDUP
   ffmpeg -i "$video"                   \
         -acodec libmp3lame             \
         -ab 320k                       \
         -ac 2                          \
         -vn                            \
-        -y "$video_title".mp3
+        -y "${title}".mp3
 
 
-  ## add image with LAME since FFMPEG changes too much....
+  ## add ${save} with LAME since FFMPEG changes too much....
   lame --preset insane -V0 --id3v2-only --ignore-tag-errors \
        --ti 'thumbnail.jpg'                                 \
        --ta "${artist}"                                     \
        --tt "${title}"                                      \
        --tv "TPE2=${artist}"                                \
        --tc "${COMMENTS}"                                   \
-   "$video_title".mp3 "${dir}/${video_title}.mp3"
+   "${title}".mp3 "${save}/${title}.mp3"
 
 
-  rm -f /tmp/*.{webm,mp4,flv,mp3,jpg,mkv} ##oikology
+  rm -f "${tmp_dir}" ## oikology...
 } || {
-  echo "You fucked up..."
+  echo -e "[fatal] The Universe doesn't give a fuck about your feelings..."
 }
